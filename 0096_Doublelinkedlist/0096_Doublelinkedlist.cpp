d9766546b0c06e13e630710524f9d5bf30ae275e
LENOVO <MeutyaAzzahrani>
2024-05-03 19:27:52 +0700

    membuat node baru

--- 0096_Doublelinkedlist/0096_Doublelinkedlist.cpp
+++ 0096_Doublelinkedlist/0096_Doublelinkedlist.cpp
@@ -5,11 +5,15 @@ struct Node {
 	int noMhs;
 	string name;
 	Node* next;
-	Node* prev
+	Node* prev;
 };
 
 Node* START = NULL;
 
 void addNode() {
-	
+	Node* newNode = new Node();
+	cout << "\nMasukkan NIM mahasiswa: ";
+	cin >> newNode->noMhs;
+	cout << "\nMasukkan nama mahasiswa: ";
+	cin >> newNode->name;
 }
